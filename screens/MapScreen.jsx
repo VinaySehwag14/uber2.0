f9f709a221b7a7846fc8d74c1d80c00f701a1c38
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-06-14 10:56:31 +0530

    added map feature of zooming out the locaitons

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -2,15 +2,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import Map from '../components/Map';
+import { createStackNavigator } from '@react-navigation/stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
   return (
     <View>
-      <Text>here is the map</Text>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
-      <View style={tw`h-1/2`}></View>
     </View>
   );
 };
